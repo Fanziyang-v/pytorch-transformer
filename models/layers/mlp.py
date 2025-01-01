e58a06b6f8f98adffb7ccc3720740c037a896165
ziyangfan <1229417754@qq.com>
2025-01-02 00:35:07 +0800

    change a param name.

--- models/layers/mlp.py
+++ models/layers/mlp.py
@@ -4,12 +4,12 @@ from torch import nn, Tensor
 class MLP(nn.Module):
     """Multi-Layer Perceptron with one hidden layer."""
 
-    def __init__(self, d_model: int, d_hidden: int, dropout: float = 0.1) -> None:
+    def __init__(self, d_model: int, d_hidden: int, drop_prob: float = 0.1) -> None:
         super(MLP, self).__init__()
         self.linear1 = nn.Linear(d_model, d_hidden)
         self.linear2 = nn.Linear(d_hidden, d_model)
         self.relu = nn.ReLU(inplace=True)
-        self.dropout = nn.Dropout(dropout)
+        self.dropout = nn.Dropout(drop_prob)
 
     def forward(self, x: Tensor) -> Tensor:
         out = self.linear1(x)
